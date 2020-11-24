958d3f78cecbd6377637d3b6ae19a0793240c232
moroojF <morooj.f2020@gmail.com>
2020-11-24 16:55:38 +0300

    inital commit

---
+++ OneDrive/سطح المكتب/CodingDojo/MERN-stack/Assignments/first-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<div>
+      <h1>Hello Dojo!</h1>
+      <h4>Things Ineed to do:</h4>
+      <div><ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul></div>
+      </div>
+  );
+}
+
+export default App;
